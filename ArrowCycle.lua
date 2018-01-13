dda0c4eb8a69cbd3776fd63117d590f3269d8c53
Raine <26597492+rainestormee@users.noreply.github.com>
2018-01-13 01:06:34 +0000

    Create ArrowCycle.lua

---
+++ ArrowCycle.lua
@@ -0,0 +1,13 @@
+--[ Configuration ]--
+timeWait = 1 --[ The time to wait to tick the lighting. Recommended 1.
+timeChange = .025 --[ The time changed at once. Recommended .025
+
+
+local lighting = game:GetService("Lighting")
+lighting:setMinutesAfterMidnight(0)
+while true do
+	print "ping"
+	lighting:setMinutesAfterMidnight(lighting:getMinutesAfterMidnight() + timeChange)
+	wait(timeWait)
+end
+--[ https://github/
